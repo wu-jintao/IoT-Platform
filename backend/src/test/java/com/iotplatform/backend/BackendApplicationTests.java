efbd4c0999a6c25fd57011b71c1dfe96af700493
wu-jintao <wujintao1211@foxmail.com>
2023-12-18 15:29:09 +0800

    配置好了mongodb

--- backend/src/test/java/com/iotplatform/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/iotplatform/backend/BackendApplicationTests.java
@@ -1,19 +1,29 @@
 package com.iotplatform.backend;
 
+import com.iotplatform.backend.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
     @Test
-    void contextLoads() {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        System.out.println(passwordEncoder.encode("123456"));
-        System.out.println(passwordEncoder.matches("123456", "$2a$10$cy8gCc1UWAwr0PTgTDBYI.f2FtuR1TBM36VMg6L/QkMHf2T2CnPNW"));
-        System.out.println(passwordEncoder.encode("w123456"));
+    public void testSaveUser() {
+        User user=new User();
+        user.setId(1);
+        user.setPassword("123456");
+        user.setDateJoined(LocalDateTime.now());
+        user.setLastLogin(LocalDateTime.now());
+        user.setUsername("赵一");
+        user.setEmail("zhaoyi@163.com");
+        mongoTemplate.save(user);
     }
-
 }
